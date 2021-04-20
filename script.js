82c221e4dfcf10d924738a22e4977ef20cfe0d04
Rubenildo Marques <82522946+Marques-Rubenildo@users.noreply.github.com>
2021-04-20 18:31:59 -0300

    versão final

--- script.js
+++ script.js
@@ -46,10 +46,11 @@ function iniciarJogo(){
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].x < 0 && direction == "up") snake[0].y = 16 * box;
 
+    //encerra o jogo
     for(i=1; i < snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
             clearInterval(jogo);
-            alert('GAME-OVER! :(');
+            alert('GAME-OVER!');
         }
     }
 
@@ -68,7 +69,8 @@ function iniciarJogo(){
     if(snakeX != food.x || snakeY != food.y){
         snake.pop();
     }
-    else{food.x = Math.floor(Math.random() * 15 + 1) * box,
+    else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box,
         food.y = Math.floor(Math.random() * 15 + 1) * box
 
     }
