b74cb2546df7b0cf9d0ab413578291e041014f45
Rubenildo Marques <82522946+Marques-Rubenildo@users.noreply.github.com>
2021-04-20 16:40:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // declarando variáveis locais
-let canvas = document.getElementById('snake');
-let context = canvas.getContext('2d');
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
 snake[0] = {
@@ -22,12 +22,26 @@ function criarCobrinha() {
     }
 }
 
-function iniciarJogo() {
+document.addEventListener('keydown', update);
+
+function update (event) {
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+function iniciarJogo(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].x < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
 
     let snakeX = snake[0].x;
-    let snakeY = sneke[0].y;
+    let snakeY = snake[0].y;
 
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
@@ -45,4 +59,3 @@ function iniciarJogo() {
 }
 
 let jogo = setInterval(iniciarJogo, 100);
-
